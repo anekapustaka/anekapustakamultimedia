5d0acc8bc4731b3c85e2a2892214ddea825d1395
anekapustaka <databaseaneka@gmail.com>
2025-10-11 20:24:43 +0700

    Update data-buku.js

--- data-buku.js
+++ data-buku.js
@@ -19,13 +19,7 @@ window.dataBuku = [
     halaman: "vi + 152 halaman",
     tahun: "2025",
     gambar: "penjas-mts.jpg",
-    sinopsis: `Buku Pendidikan Jasmani, Olahraga, dan Kesehatan untuk kelas VII dirancang sebagai panduan lengkap untuk membantu siswa mengembangkan keterampilan gerak, 
-    strategi olahraga, kerja sama tim, dan kebugaran jasmani. Materi disajikan secara sistematis mulai dari dasar hingga 
-    penerapan praktis, dilengkapi dengan uji kompetensi, pengayaan, dan refleksi untuk mempermudah pemahaman. Selain aspek fisik, 
-    buku ini juga menekankan nilai karakter seperti disiplin, sportivitas, dan tanggung jawab, sehingga peserta didik tidak hanya 
-    menjadi lebih sehat dan bugar, tetapi juga memiliki sikap positif yang mendukung perkembangan pribadi dan sosial. Dengan pendekatan 
-    yang menarik dan aplikatif, buku ini mendorong siswa untuk aktif bergerak, berpikir kritis, dan berpartisipasi secara penuh dalam kegiatan jasmani, 
-    sekaligus menyiapkan mereka menjadi generasi muda yang sehat, cerdas, dan berkarakter.`
+    sinopsis: `Buku Pendidikan Jasmani, Olahraga, dan Kesehatan untuk kelas VII dirancang sebagai panduan lengkap untuk membantu siswa mengembangkan keterampilan gerak, strategi olahraga, kerja sama tim, dan kebugaran jasmani. Materi disajikan secara sistematis mulai dari dasar hingga penerapan praktis, dilengkapi dengan uji kompetensi, pengayaan, dan refleksi untuk mempermudah pemahaman. Selain aspek fisik, buku ini juga menekankan nilai karakter seperti disiplin, sportivitas, dan tanggung jawab, sehingga peserta didik tidak hanya menjadi lebih sehat dan bugar, tetapi juga memiliki sikap positif yang mendukung perkembangan pribadi dan sosial. Dengan pendekatan yang menarik dan aplikatif, buku ini mendorong siswa untuk aktif bergerak, berpikir kritis, dan berpartisipasi secara penuh dalam kegiatan jasmani, sekaligus menyiapkan mereka menjadi generasi muda yang sehat, cerdas, dan berkarakter.`
   },
   {
     id: "senirupa-mts-vii",
@@ -44,7 +38,82 @@ window.dataBuku = [
     halaman: "vi + 118 halaman",
     tahun: "2025",
     gambar: "senirupa-mts.png",
-    sinopsis: `Buku Seni Rupa Kelas VII SMP/MTs (Kurikulum Merdeka) ini disusun untuk membimbing peserta didik mengenal, memahami, dan mengembangkan kreativitas dalam berkarya seni rupa secara menyeluruh. Melalui empat bab utama—Menggambar, Mendesain, Membuat Tipografi dan Logo, serta Berkarya Seni untuk Perubahan—buku ini mengajak siswa untuk mengekspresikan ide, perasaan, dan gagasan melalui berbagai media dan teknik. Selain mempelajari unsur dan prinsip seni rupa, peserta didik juga diarahkan untuk peka terhadap lingkungan sosial, budaya, dan kemanusiaan di sekitarnya. Buku ini diharapkan dapat menumbuhkan kemampuan berpikir kreatif, kritis, dan kolaboratif, sekaligus menanamkan nilai estetika, empati, serta tanggung jawab sosial melalui kegiatan seni yang bermakna..`
+    sinopsis: `Buku Seni Rupa Kelas VII SMP/MTs (Kurikulum Merdeka) ini disusun untuk membimbing peserta didik mengenal, memahami, dan mengembangkan kreativitas dalam berkarya seni rupa secara menyeluruh. Melalui empat bab utama—Menggambar, Mendesain, Membuat Tipografi dan Logo, serta Berkarya Seni untuk Perubahan—buku ini mengajak siswa untuk mengekspresikan ide, perasaan, dan gagasan melalui berbagai media dan teknik. Selain mempelajari unsur dan prinsip seni rupa, peserta didik juga diarahkan untuk peka terhadap lingkungan sosial, budaya, dan kemanusiaan di sekitarnya. Buku ini diharapkan dapat menumbuhkan kemampuan berpikir kreatif, kritis, dan kolaboratif, sekaligus menanamkan nilai estetika, empati, serta tanggung jawab sosial melalui kegiatan seni yang bermakna.`
+  },
+  {
+    id: "informatika-mts-vii",
+    judul: "Informatika (Untuk Siswa SMP/MTs Kelas VII)",
+    harga: "Rp. 89.250",
+    isbn: "-",
+    penulis: [
+      "Dr. H. Dasmadi, S.E., M.M",
+      "Hj. Yunita Niqrisah Dwi Pratiwi, S.E., M.Si",
+      "Toni Siswanto, S.E., M.M",
+      "Rafliditya Bayu Wicaksono",
+      "Hj. Siti Syamsiyah, S.Pd., M.Pd"
+    ],
+    editor: "Prof. Dr. H. Zaidan Nawawi, M.Si",
+    ukuran: "17,6 X 25 cm",
+    halaman: "vi + 144 halaman",
+    tahun: "2025",
+    gambar: "informatika-mts-vii.png",
+    sinopsis: `Buku "Modul Ajar Informatika" ini disusun sebagai panduan pembelajaran bagi peserta didik kelas VII SMP/MTs untuk memahami konsep-konsep dasar informatika secara sistematis dan terstruktur. Modul ini mengintegrasikan teori dan praktik yang menekankan pada pengembangan kompetensi berpikir kritis, kreatif, komunikatif, dan kolaboratif. Materi dalam buku ini mencakup berbagai topik utama, antara lain pengenalan data dan informasi, dasar-dasar algoritma dan pemrograman, konsep komputasi, jaringan komputer, sistem operasi, serta prinsip-prinsip etika digital. Setiap bab dirancang untuk memperkenalkan peserta didik pada dunia teknologi informasi dengan bahasa yang komunikatif dan pendekatan pembelajaran yang kontekstual, sehingga peserta didik dapat mengaitkan teori informatika dengan kehidupan sehari-hari. Buku ini dilengkapi dengan berbagai fitur pendukung pembelajaran seperti ilustrasi visual, contoh aplikasi nyata, latihan soal, tugas proyek berbasis masalah (problem-based project), refleksi diri, dan asesmen formatif. Dengan demikian, peserta didik tidak hanya memahami konsep secara kognitif, tetapi juga mengembangkan keterampilan praktis dan karakter positif dalam menggunakan teknologi informasi.`
+  },
+  {
+    id: "ips-mts-vii",
+    judul: "Ilmu Pengetahuan Sosial (Untuk Siswa SMP/MTs Kelas VII)",
+    harga: "Rp. 87.150",
+    isbn: "-",
+    penulis: [
+      "Hj. Yunita Niqrisah Dwi Pratiwi, S.E., M.Si",
+      "Toni Siswanto, S.E., M.M",
+      "Hj. Siti Syamsiyah, S.Pd., M.Pd",
+      "Dr. H. Dasmadi, S.E., M.M",
+      "Heri Irwanto, S.M"
+    ],
+    editor: "Prof. Dr. H. Zaidan Nawawi, M.Si",
+    ukuran: "17,6 X 25 cm",
+    halaman: "vi + 138 halaman",
+    tahun: "2025",
+    gambar: "ips-mts-vii.png",
+    sinopsis: `Buku Ilmu Pengetahuan Sosial (IPS) untuk Kelas VII ini disusun berdasarkan Kurikulum Merdeka dengan tujuan membantu siswa memahami berbagai fenomena sosial, ekonomi, geografi, dan sejarah yang berkaitan dengan kehidupan sehari-hari. Materi dalam buku ini disajikan secara sistematis, mulai dari pengenalan manusia dan lingkungan, interaksi sosial, kegiatan ekonomi, hingga pemberdayaan masyarakat, sehingga siswa mampu melihat keterkaitan antara konsep IPS dengan realitas yang mereka hadapi. Melalui pendekatan pembelajaran aktif, projek, dan aktivitas kontekstual, buku ini  mendorong siswa berpikir kritis, kreatif, serta memiliki karakter sesuai profil pelajar Pancasila. Dengan demikian, buku ini diharapkan tidak hanya menjadi sumber belajar, tetapi juga sarana pembentukan sikap, keterampilan, dan wawasan kebangsaan yang kuat pada diri peserta didik.`
+  },
+  {
+    id: "pkn-mts-vii",
+    judul: "Pendidikan Kewarganegaraan (Untuk Siswa SMP/MTs Kelas VII)",
+    harga: "Rp. 88.550",
+    isbn: "-",
+    penulis: [
+      "Hj. Siti Syamsiyah, S.Pd., M.Pd",
+      "Toni Siswanto, S.E., M.M",
+      "Hj. Yunita Niqrisah Dwi Pratiwi, S.E., M.Si",
+      "Dr. H. Dasmadi, S.E., M.M",
+      "Abid Khotibul Umam, S.M"
+    ],
+    editor: " Prof. Dr. KH. Abdul Matin Bin Salman, Lc., M.Ag.",
+    ukuran: "17,6 X 25 cm",
+    halaman: "viii + 140 halaman",
+    tahun: "2025",
+    gambar: "pkn-mts.jpg",
+    sinopsis: `Buku Pendidikan Kewarganegaraan Kelas VII ini disusun sebagai panduan belajar siswa dalam memahami dan mengamalkan nilai-nilai kebangsaan, demokrasi, hak asasi manusia, serta etika dalam kehidupan bernegara. Melalui pendekatan yang kontekstual dan interaktif, buku ini membahas topik-topik penting seperti Pancasila, UUD 1945, kewarganegaraan, partisipasi sosial, hingga perilaku etis di masyarakat dan media sosial. Disusun sesuai Kurikulum Nasional 2025, buku ini tidak hanya membekali siswa dengan pengetahuan, tetapi juga mendorong mereka untuk menjadi warga negara yang aktif, kritis, bertanggung jawab, dan berintegritas dalam membangun bangsa Indonesia.`
+  },
+  {
+    id: "ipa-mts-vii",
+    judul: "Ilmu Pengetahuan Alam (Untuk Siswa SMP/MTs Kelas VII)",
+    harga: "Rp. 98.350",
+    isbn: "-",
+    penulis: [
+      "Dr. H. Dasmadi, S.E., M.M",
+      "Hj. Yunita Niqrisah Dwi Pratiwi, S.E., M.Si",
+      "Toni Siswanto, S.E., M.M",
+      "Reza Estati Putri, S.Pd.",
+      "Hj. Siti Syamsiyah, S.Pd., M.Pd"
+    ],
+    editor: "Prof. Dr. H. Zaidan Nawawi, M.Si",
+    ukuran: "17,6 X 25 cm",
+    halaman: "vi + 170 halaman",
+    tahun: "2025",
+    gambar: "ipa-mts-vii.jpeg",
+    sinopsis: `Buku Ilmu Pengetahuan Alam (IPA) Kelas VII ini disusun untuk membantu peserta didik memahami konsep-konsep dasar sains yang meliputi makhluk hidup, ekosistem, zat dan perubahannya, energi, sistem bumi, serta tata surya. Melalui pendekatan kontekstual dan kegiatan eksperimen sederhana, buku ini tidak hanya menyajikan pengetahuan faktual, tetapi juga menumbuhkan rasa ingin tahu, berpikir kritis, dan keterampilan ilmiah. Disajikan secara menarik dengan ilustrasi, rangkuman, soal latihan, dan aktivitas praktikum, buku ini diharapkan dapat memotivasi siswa untuk lebih dekat dengan alam sekitar, memahami fenomena kehidupan sehari-hari, serta menumbuhkan sikap peduli terhadap lingkungan dan kebesaran ciptaan Tuhan.`
   }
-
 ];
