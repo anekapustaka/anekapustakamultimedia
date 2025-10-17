9be08510f96dfc32e4a8e9bc0032b5770a74ef2f
anekapustaka <databaseaneka@gmail.com>
2025-10-17 13:58:53 +0700

    Create data-artikel.js

---
+++ data-artikel.js
@@ -0,0 +1,29 @@
+// data-artikel.js
+// ukuran gambar rekomendasi 1200x400 px
+
+const articles = {
+  "1": {
+    title: "Cara Menerbitkan Buku Anak di Indonesia",
+    date: "17 Oktober 2025",
+    author: "Admin Aneka Pustaka Multimedia",
+    banner: "images/logocv.png",
+    description: "Panduan lengkap menerbitkan buku anak di Indonesia, dari naskah hingga distribusi.",
+    keywords: "buku anak, penerbitan, literasi anak, Aneka Pustaka Multimedia",
+    content: `
+      <p>Menerbitkan buku anak memerlukan strategi khusus mulai dari penulisan naskah hingga ilustrasi.</p>
+      <p>Pilih penerbit yang berpengalaman dalam literasi anak agar hasil maksimal.</p>
+    `
+  },
+  "2": {
+    title: "Tips Menulis Novel Pendek",
+    date: "10 Oktober 2025",
+    author: "Admin Aneka Pustaka Multimedia",
+    banner: "images/logocv.png", 
+    description: "Tips praktis menulis novel pendek dengan karakter dan plot yang kuat.",
+    keywords: "novel pendek, menulis, tips menulis, Aneka Pustaka Multimedia",
+    content: `
+      <p>Novel pendek membutuhkan fokus cerita dan karakter yang kuat. Mulailah dengan outline yang jelas.</p>
+      <p>Latih konsistensi menulis harian untuk menyelesaikan naskah tepat waktu.</p>
+    `
+  }
+};
