dcb6eef3095f339b9d862c2b51a4782a5fea51a9
RAFLI <rfuserbot@gmail.com>
2026-01-23 15:07:49 +0700

    add pdfjs

---
+++ script/data-repo.js
@@ -0,0 +1,24 @@
+window.dataRepo = [
+  {
+    id: "iman-dalam-irama",
+
+    judul: "Iman dalam Irama | Resonansi Religiusitas Musik dalam Islam",
+
+    penulis: "Dr. Abdul Matin bin Salman, Lc., M.Ag",
+
+    editor: [
+      "Prof. Dr. H. Zaidan Nawawi, M.Si",
+	  "Dr. H. Dasmadi, S.E., M.M."
+    ],
+
+    tahun: "2024",
+
+    isbn: "978-623-8752-19-5",
+
+    cover: "imandalamirama.png",
+
+    file: "Iman dalam Irama, Resonansi Religiusitas Musik dalam Islam.pdf",
+
+    sinopsis: "Setiap bab di dalam buku ini, akan disajikan bagaimana musik diintegrasikan ke dalam praktik keagamaan dan kehidupan seharihari umat Islam. Buku ini menyajikan analisis mendalam mengenai bagaimana musik, dalam berbagai bentuknya, telah diterima dan digunakan oleh masyarakat Muslim sepanjang sejarah. Mulai dari syair-syair yang mengiringi ibadah hingga nasyid dan qasidah yang menggugah semangat spiritual, kita akan melihat betapa luasnya spektrum musik dalam Islam"
+  }
+];
