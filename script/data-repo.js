ddcd41e8b641cd3f69a05fe5eeffacf51038c89b
RAFLI <rfuserbot@gmail.com>
2026-02-09 11:10:46 +0700

    upload buku

--- script/data-repo.js
+++ script/data-repo.js
@@ -20,5 +20,32 @@ window.dataRepo = [
     file: "Iman dalam Irama, Resonansi Religiusitas Musik dalam Islam.pdf",
 
     sinopsis: "Setiap bab di dalam buku ini, akan disajikan bagaimana musik diintegrasikan ke dalam praktik keagamaan dan kehidupan seharihari umat Islam. Buku ini menyajikan analisis mendalam mengenai bagaimana musik, dalam berbagai bentuknya, telah diterima dan digunakan oleh masyarakat Muslim sepanjang sejarah. Mulai dari syair-syair yang mengiringi ibadah hingga nasyid dan qasidah yang menggugah semangat spiritual, kita akan melihat betapa luasnya spektrum musik dalam Islam"
-  }
+  },
+  
+  {
+    id: "green-leadership-in-the-hospitality-industry-strategies-for-building-sustainable-employee-performance",
+
+    judul: "Green Leadership in the Hospitality Industry Strategies for Building Sustainable Employee Performance",
+
+    penulis: [
+	  "Tedy Setiawan Saputra, S.E.,M.M.,CRMPA.,CACP",
+	  "Prof. Dr. Zaenal Arifin, M.Si",
+	  "Dr. Majang Palupi, BBA,MBA"
+	],
+
+    editor: [
+		"Dr. H. Dasmadi, S.E.,M.M",
+		"Toni Siswanto, S.E.,M.M",
+		"Hj. Yunita Niqrisah Dwi Pratiwi, S.E.,M.Si"
+    ],
+
+    tahun: "2025",
+
+    isbn: "978-623-8752-98-0",
+
+    cover: "greenleadership.jpeg",
+
+    file: "Green Leadership in the Hospitality Industry Strategies for Building Sustainable Employee Performance.pdf",
+
+    sinopsis: "Buku ini mengulas secara komprehensif peran green leadership dalam membangun kinerja karyawan yang berkelanjutan di industri perhotelan melalui sinergi green human resource management dan employee involvement. Dengan memadukan pendekatan konseptual dan praktik aplikatif, buku ini menjelaskan bagaimana kepemimpinan yang berorientasi lingkungan mampu mendorong perubahan perilaku, memperkuat keterlibatan karyawan, serta menumbuhkan budaya organisasi hijau. Berbagai pembahasan strategis, praktik terbaik, dan arah masa depan industri perhotelan berkelanjutan disajikan sebagai rujukan bagi akademisi dan praktisi dalam merancang kebijakan dan strategi pengelolaan sumber daya manusia yang adaptif, bertanggung jawab, dan berdaya saing jangka panjang."  },
 ];
